56dfa515f3ddb146d9c51f69bfcc684920cbf382
chobits tai <chobits@itri.org.tw>
2026-02-05 15:22:06 +0800

    adjust sensor gain

--- auto_exp.py
+++ auto_exp.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from std_msgs.msg import Float32
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy
 import numpy as np
 import cv2
@@ -37,6 +38,9 @@ class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('auto_exp')
         self.subscription = self.create_subscription(Image, '/mono_left_unsync', self.callback, QoSProfile(depth=1, reliability=QoSReliabilityPolicy.BEST_EFFORT))
+        self.gain_pub = self.create_publisher(Float32, "camera/gain", QoSProfile(depth=1, reliability=QoSReliabilityPolicy.BEST_EFFORT))
+        self.gain = 1.0
+        self.MAX_GAIN = 4.0
         self.cnt = 0
         self.shutter_spd_us = 10000
         self.ser = serial.Serial('/dev/ttyAMA2', 921600)
@@ -50,22 +54,33 @@ class ImageSubscriber(Node):
     def __del__(self):
         self.close()
     def callback(self, msg):
-        self.cnt = self.cnt + 1
+        self.cnt += 1
         if self.cnt > 4:
             self.cnt = 0;
             flat_img = np.frombuffer(msg.data, dtype=np.uint8)
             avg_bright = get_safe_exposure_level(flat_img)
-            #avg_bright = np.mean(img)
             if avg_bright <= 100 or avg_bright >= 120:
                 adj = 1 + (110 / avg_bright - 1) * 0.1
-                self.shutter_spd_us = int(self.shutter_spd_us * adj)
-                if self.shutter_spd_us > 20000: # 40fps exposure time limit
-                    self.shutter_spd_us = 20000
-                elif self.shutter_spd_us < 500:
-                    self.shutter_spd_us = 500
-                print(self.shutter_spd_us, " us")
-                self.ser.write(struct.pack('<HH', self.shutter_spd_us, self.shutter_spd_us))
-                self.ser.flush()
+                if adj < 1.0 and self.gain > 1.0:
+                    self.gain *= 0.9
+                    chg_gain = Float32()
+                    chg_gain.data = self.gain
+                    self.gain_pub.publish(chg_gain)
+                else:
+                    self.shutter_spd_us = int(self.shutter_spd_us * adj)
+                    if self.shutter_spd_us > 10000: # avoid motion blur
+                        self.shutter_spd_us = 10000
+                        self.gain *= 1.1
+                        if self.gain > self.MAX_GAIN:
+                            self.gain = self.MAX_GAIN
+                        chg_gain = Float32()
+                        chg_gain.data = self.gain
+                        self.gain_pub.publish(chg_gain)
+                    elif self.shutter_spd_us < 500:
+                        self.shutter_spd_us = 500
+                    print(self.shutter_spd_us, " us", "gain:", self.gain)
+                    self.ser.write(struct.pack('<HH', self.shutter_spd_us, self.shutter_spd_us))
+                    self.ser.flush()
 
 def main(args=None):
     rclpy.init(args=args)
